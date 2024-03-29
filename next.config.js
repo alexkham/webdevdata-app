554683342b22e6a1b43bada004d718ce4d2fcdf1
alexkham <alexkha@netvision.net.il>
2024-03-29 20:37:23 +0300

    config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
 
     reactStrictMode: false,
-    experimental: {
-        // Enable the Server Components feature flag
-        serverComponents: true,
-        // Specifically for enabling Server Actions, as of my last update
-        // Check Next.js documentation for the most current flags and names
-        appDir: true,
-      },
+    // experimental: {
+    //     // Enable the Server Components feature flag
+    //     serverComponents: true,
+    //     // Specifically for enabling Server Actions, as of my last update
+    //     // Check Next.js documentation for the most current flags and names
+    //     appDir: true,
+    //   },
 }
 
 module.exports = nextConfig
